d70da9c3ec4466a1a1ac328c5a073499c3a5f6e5
Cristian Metz <cristian@mobilityps.com>
2021-03-14 13:22:37 -0300

    class 32

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';//NOTE: The name HAS to be uppercase first. If not React does not think it is a custom component
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
        <h1>
         Hola mundo!
        </h1> I'm allowed to do this cause its nested inside the main element
+        <Person></Person>
       </div>
     );
     //return(React.createElement('div',{className:'App'},React.createElement('h1',null,'Hola mundo')));//this is the equivalent to the other code.
