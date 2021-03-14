b08ed890c5ec4bbb6aee8fcb0caa8804e78f1e3b
Cristian Metz <cristian@mobilityps.com>
2021-03-14 13:26:37 -0300

    class 33

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ class App extends Component {
        <h1>
         Hola mundo!
        </h1> I'm allowed to do this cause its nested inside the main element
-        <Person></Person>
+       <Person />
+       <Person />
+       <Person />
+       <Person />
+       <Person />
       </div>
     );
     //return(React.createElement('div',{className:'App'},React.createElement('h1',null,'Hola mundo')));//this is the equivalent to the other code.
