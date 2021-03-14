600ab496b1045cd32843bebd4c4e6626b3c29bad
Cristian Metz <cristian@mobilityps.com>
2021-03-14 13:05:31 -0300

    class 30

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          cursoide de react amigoide epico lol
-          
-        </p>
+       <h1>
+        a
+       </h1> //THIS IS JSX
       </div>
     );
+    //return(React.createElement('div',{className:'App'},React.createElement('h1',null,'Hola mundo')));//this is the equivalent to the other code.
   }
 }
 
