7c3b42dea0d2c6671332ae5d4549d306ec33f625
Cristian Metz <cristian@mobilityps.com>
2021-03-15 16:47:03 -0300

    class 38

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import Person from './Person/Person.js';//NOTE: The name HAS to be uppercase first. If not React does not think it is a custom component
 
 class App extends Component {
+  state ={
+    persons: [
+      { name: 'Max', age:'28' },
+      {name:'Lulu', age:'15'},
+      {name:'Manu', age:'40'}
+
+    ]
+  }
   render() {
     return (
       <div className="App">
@@ -10,10 +18,10 @@ class App extends Component {
         Hola mundo!
        </h1> I'm allowed to do this cause its nested inside the main element
       <br></br>
-       <Person name='Max' age='28'/>
-       <Person name='Lulu' age='15'>My hobby is to play videogames!</Person>
-       <Person name='Manu' age='40'/>
-       <Person name='Juan' age='30'/>
+       <button> Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is to play videogames!</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return(React.createElement('div',{className:'App'},React.createElement('h1',null,'Hola mundo')));//this is the equivalent to the other code.
