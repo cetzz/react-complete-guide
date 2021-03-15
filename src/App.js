7cf414abd57b3ab509a8c169230efd147426d2b1
cetzz <cristiangerma@hotmail.com>
2021-03-15 20:27:16 -0300

    WIP Class 44

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js';//NOTE: The name HAS to be uppercase first. If not React does not think it is a custom component
 
-class App extends Component {
-  state ={
+const app = props =>{
+  const [ personsState, setPersonsState] = useState({
     persons: [
-      { name: 'Max', age:'28' },
+    { name: 'Max', age:'28' },
+    {name:'Lulu', age:'15'},
+    {name:'Manu', age:'40'}
+
+  ],
+  otherState:'some other value'
+});
+const switchNameHandler = () =>{
+  setPersonsState({
+    persons: [
+      { name: 'Maximilian', age:'28' },
       {name:'Lulu', age:'15'},
-      {name:'Manu', age:'40'}
+      {name:'Manu', age:'41'}
 
-    ],
-    otherState:'some other value'
-  }
-
-  switchNameHandler = () =>{
-    //DONT DO THIS: this.state.persons[0].name='lol';
-    this.setState({
-      persons: [
-        { name: 'Maximilian', age:'28' },
-        {name:'Lulu', age:'15'},
-        {name:'Manu', age:'41'}
-  
-      ]
-    });
-  }
+    ]
+  });
+}
 
   render() {
     return (
@@ -32,14 +30,36 @@ class App extends Component {
         Hola mundo!
        </h1> I'm allowed to do this cause its nested inside the main element
       <br></br>
-      <p> <button onClick={this.switchNameHandler}> Switch Name</button></p>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is to play videogames!</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <p> <button onClick={switchNameHandler}> Switch Name</button></p>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobby is to play videogames!</Person>
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     //return(React.createElement('div',{className:'App'},React.createElement('h1',null,'Hola mundo')));//this is the equivalent to the other code.
   }
 }
 
-export default App;
+export default app;
+
+state ={
+  persons: [
+    { name: 'Max', age:'28' },
+    {name:'Lulu', age:'15'},
+    {name:'Manu', age:'40'}
+
+  ],
+  otherState:'some other value'
+}
+
+switchNameHandler = () =>{
+  //DONT DO THIS: personsState.persons[0].name='lol';
+  personsSetState({
+    persons: [
+      { name: 'Maximilian', age:'28' },
+      {name:'Lulu', age:'15'},
+      {name:'Manu', age:'41'}
+
+    ]
+  });
+}
