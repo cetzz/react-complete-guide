e81babbac7710ad5a75ac0a562beb3b6b888f77b
Cristian Metz <cristian@mobilityps.com>
2021-03-15 16:23:01 -0300

    class 35

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = ()=>{
-    return <p> I'm a Person!</p>
-}
-
+    return <p> I'm a Person! and I am {(Math.round(Math.random())==1?Math.floor(Math.random()*30):'This works too')}</p>
+}//ternaries can also work btw. But the important part of this is that we need { }
+//to output code
 export default person;
