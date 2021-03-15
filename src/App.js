b528320e27bcd3f9a8670c1c48ce9eeeff451734
Cristian Metz <cristian@mobilityps.com>
2021-03-15 18:34:10 -0300

    class 42

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ class App extends Component {
       {name:'Lulu', age:'15'},
       {name:'Manu', age:'40'}
 
-    ]
+    ],
+    otherState:'some other value'
   }
+
+  switchNameHandler = () =>{
+    //DONT DO THIS: this.state.persons[0].name='lol';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age:'28' },
+        {name:'Lulu', age:'15'},
+        {name:'Manu', age:'41'}
+  
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,7 +32,7 @@ class App extends Component {
         Hola mundo!
        </h1> I'm allowed to do this cause its nested inside the main element
       <br></br>
-       <button> Switch Name</button>
+      <p> <button onClick={this.switchNameHandler}> Switch Name</button></p>
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is to play videogames!</Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
